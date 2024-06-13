a990ebec7b877e22de9b38af3c97a8424e4b9782
saatarko <saatar@tut.by>
2024-06-13 20:41:17 +0300

    Django 8. fix css and html

--- homejango/homejango/settings.py
+++ homejango/homejango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1n+=o9h!ffgb+%t@)oa5%p6--1ikpo!tv(!x63@1-iy1lg-h&s'
+# SECRET_KEY = 'django-insecure-1n+=o9h!ffgb+%t@)oa5%p6--1ikpo!tv(!x63@1-iy1lg-h&s'
+SECRET_KEY = os.environ.get('Django_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
