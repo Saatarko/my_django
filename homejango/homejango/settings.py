88bbed3b95285bd6b3f4662fbe1ecea33c691da1
saatarko <saatar@tut.by>
2024-06-16 22:14:52 +0300

    Django 8. fix css and html

--- homejango/homejango/settings.py
+++ homejango/homejango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('Django_SECRET_KEY')
+SECRET_KEY = 'django-insecure-1n+=o9h!ffgb+%t@)oa5%p6--1ikpo!tv(!x63@1-iy1lg-h&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- homejango/service/admin.py
+++ homejango/service/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
 
 from .models import Clients, Vacations, Doctor, Procedure, Pets, Order
+
+
 # Register your models here.
 
-admin.site.register(Clients)   #регистрируем таблицы для вывода в админке
+@admin.register(Clients)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone')
+    list_display_links = ('id', 'first_name', 'last_name')      # поля по которым можно переходить в админке
+
+
+@admin.register(Pets)
+class PetsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nickname', 'birthdate', 'breed', 'color', 'clients')
+    list_display_links = ('id', 'nickname')
+    list_editable = ('birthdate', 'breed', 'color',)      # редактируемые поля
+    # редактируемое поле не может быть кликабельным т.е в list_display_links и list_editable не должны пересекаться
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_order', 'time_order', 'procedure', 'clients', 'doctor')
+    list_display_links = ('id', 'date_order', 'time_order', 'procedure', 'clients', 'doctor')
+    ordering = ['date_order', 'time_order']   # сортировка записей только для админки
+    list_per_page = 5  # Указывает какое кол-во данных будет на одной странице
+
+
+# admin.site.register(Clients, ClientAdmin)  #регистрируем таблицы для вывода в админке но лучше через декоратор
 admin.site.register(Vacations)
 admin.site.register(Doctor)
 admin.site.register(Procedure)
-admin.site.register(Pets)
-admin.site.register(Order)
+# admin.site.register(Pets, PetsAdmin)
+# admin.site.register(Order,OrderAdmin)
